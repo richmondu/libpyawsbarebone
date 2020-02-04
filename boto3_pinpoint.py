3ffd2032c59c8391671f28d6694bda0de276860c
richmondu <richmond.umagat@yahoo.com>
2020-02-04 16:28:02 +0800

    Update boto3_pinpoint.py

--- boto3_pinpoint.py
+++ boto3_pinpoint.py
@@ -20,6 +20,20 @@ g_region_name         = "us-east-1"                                 # UPDATE ME
 g_email_from          = "richmond.umagat@gmail.com"                 # UPDATE ME
 ###############################################################################
 
+###############################################################################
+g_push_notification_service_android = "GCM"
+g_push_notification_service_apple   = "APNS"
+g_push_notification_service_use     = g_push_notification_service_apple
+g_push_notification_token_android   = ""                            # UPDATE ME
+g_push_notification_token_apple     = ""                            # UPDATE ME
+###############################################################################
+
+###############################################################################
+g_send_sms = False
+g_send_email = False
+g_send_push_notification = True
+###############################################################################
+
 
 
 ###############################################################################
@@ -60,12 +74,66 @@ def send_sms(pinpoint, sms_recipient, sms_message):
     )
     return response
 
+###############################################################################
+# https://docs.aws.amazon.com/pinpoint/latest/developerguide/send-messages-push.html
+###############################################################################
+def send_push_notification(pinpoint, recipient, message, title):
+    print("send_push_notification")
+    action = "URL"
+    url = "https://www.example.com"
+    priority = "normal"
+    ttl = 30
+    silent = False
+    token = recipient["token"]
+    service = recipient["service"]
 
-def send_message(pinpoint, recipient, message, subject=None):
-    if subject is None:
+    if service == g_push_notification_service_android:
+        response = pinpoint.send_messages(
+            ApplicationId=g_pinpoint_project_id,
+            MessageRequest={
+                'Addresses': {token: {'ChannelType': service}},
+                'MessageConfiguration': {
+                    'GCMMessage': {
+                        'Action': action,
+                        'Body': message,
+                        'Priority' : priority,
+                        'SilentPush': silent,
+                        'Title': title,
+                        'TimeToLive': ttl,
+                        'Url': url
+                    }
+                }
+            }
+        )
+    elif service == g_push_notification_service_apple:
+        response = pinpoint.send_messages(
+            ApplicationId=g_pinpoint_project_id,
+            MessageRequest={
+                'Addresses': {token: {'ChannelType': service}},
+                'MessageConfiguration': {
+                    'APNSMessage': {
+                        'Action': action,
+                        'Body': message,
+                        'Priority' : priority,
+                        'SilentPush': silent,
+                        'Title': title,
+                        'TimeToLive': ttl,
+                        'Url': url
+                    }
+                }
+            }
+        )
+    return response
+
+
+def send_message(pinpoint, type, recipient, message, subject=None):
+    if type == "sms":
         response = send_sms(pinpoint, recipient, message)
-    else:
+    elif type == "email":
         response = send_email(pinpoint, recipient, message, subject)
+    elif type == "push_notification":
+        response = send_push_notification(pinpoint, recipient, message, subject)
+
     return response
 
 
@@ -83,18 +151,43 @@ def main(args):
 
 
     # sms
-    recipient = "+639175900612"
-    response = send_message(pinpoint, recipient, message, subject=None)
-    print("Sending SMS done. {} {}".format(
-        response["ResponseMetadata"]["HTTPStatusCode"]==200, 
-        response["MessageResponse"]["Result"][recipient]["StatusCode"]==200))
+    if g_send_sms:
+        recipient = "+639175900612"
+        response = send_message(pinpoint, "sms", recipient, message, None)
+        print("Sending SMS done. {} {}".format(
+            response["ResponseMetadata"]["HTTPStatusCode"]==200, 
+            response["MessageResponse"]["Result"][recipient]["StatusCode"]==200))
 
     # email
-    recipient = "richmond.umagat@yahoo.com"
-    response = send_message(pinpoint, recipient, message, subject=subject)
-    print("Sending EMAIL done. {} {}".format(
-        response["ResponseMetadata"]["HTTPStatusCode"]==200, 
-        response["MessageResponse"]["Result"][recipient]["StatusCode"]==200))
+    if g_send_email:
+        recipient = "richmond.umagat@yahoo.com"
+        response = send_message(pinpoint, "email", recipient, message, subject)
+        print("Sending EMAIL done. {} {}".format(
+            response["ResponseMetadata"]["HTTPStatusCode"]==200, 
+            response["MessageResponse"]["Result"][recipient]["StatusCode"]==200))
+
+    # push notification
+    if g_send_push_notification:
+        if g_push_notification_service_use == g_push_notification_service_android:
+            # android
+            recipient = {
+                "token"  : g_push_notification_token_android,
+                "service": g_push_notification_service_android
+            }
+            print("android")
+        elif g_push_notification_service_use == g_push_notification_service_apple:
+            # apple
+            recipient = {
+                "token"  : g_push_notification_token_apple,
+                "service": g_push_notification_service_apple
+            }
+            print("apple")
+
+        response = send_message(pinpoint, "push_notification", recipient, message, subject)
+        print(response)
+        #print()
+        #print("Sending PUSH NOTIFICATION done. {}".format(
+        #    response['MessageResponse']['Result'][recipient]['DeliveryStatus'] == "SUCCESSFUL"))
 
     return
 
